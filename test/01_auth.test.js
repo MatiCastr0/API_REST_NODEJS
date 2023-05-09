9ad7878ea2e9d7025dd564e8e39cbffff313b5fd
Matias <maticastrook@gmail.com>
2023-05-09 12:55:51 -0300

    Testing Jest Completo

--- test/01_auth.test.js
+++ test/01_auth.test.js
@@ -1,38 +1,44 @@
 const request = require("supertest");
-
 const app = require("../app");
 const { usersModel } = require("../models");
-const testAuthLogin = {
-  email: "string",
-  password: "string",
-};
+const { testAuthLogin, testAuthRegister } = require("./helper/helperData");
 
-const testAuthRegister = {
-  email: "matias121212@gmail.com",
-  password: "123456",
-  age: 20,
-  name: "mati",
-};
-
-/* Se va a ejecutar antes de la pruebas */
 beforeAll(async () => {
-  await usersModel.deleteMany();
+  await usersModel.deleteMany({});
 });
 
-describe("[AUTH] esta es la prueba de /api/auth", () => {
-  test("Esto deberia de retornar un 403", async () => {
-    const response = await request(app)
-      .post("/api/auth/login")
-      .send(testAuthLogin);
-    expect(response.statusCode).toEqual(403);
-  });
+test("esto deberia de retornar 404", async () => {
+  const response = await request(app)
+    .post("/api/auth/login")
+    .send(testAuthLogin);
+
+  expect(response.statusCode).toEqual(404);
 });
-test("Esto deberia de retornar un 201", async () => {
+
+test("esto deberia de retornar 201", async () => {
   const response = await request(app)
     .post("/api/auth/register")
     .send(testAuthRegister);
+
   expect(response.statusCode).toEqual(201);
   expect(response.body).toHaveProperty("data");
   expect(response.body).toHaveProperty("data.token");
   expect(response.body).toHaveProperty("data.user");
+});
+
+test("esto deberia de retornar password no valido 401", async () => {
+  const newTestAuthLogin = { ...testAuthLogin, password: "22222222" };
+  const response = await request(app)
+    .post("/api/auth/login")
+    .send(newTestAuthLogin);
+
+  expect(response.statusCode).toEqual(401);
+});
+
+test("esto deberia de retornar 200 login exitoso", async () => {
+  const response = await request(app)
+    .post("/api/auth/login")
+    .send(testAuthRegister);
+
+  expect(response.statusCode).toEqual(200);
 });
--- test/app.test.js
+++
@@ -1,8 +0,0 @@
-describe("[APP] Esta es una prueba general", () => {
-  test("Esto deberia retornar", () => {
-    a = 4;
-    b = 4;
-    const total = a + b;
-    expect(total).toEqual(8);
-  });
-});
