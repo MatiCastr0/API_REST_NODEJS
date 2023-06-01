ac9b2e8e68581dc586f156e75cb029c3e8a3d2e8
Matias <maticastrook@gmail.com>
2023-06-01 10:11:48 -0300

    Actualizacion app.js

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const openApiConfiguration = require("./docs/swagger");
 const swaggerUI = require("swagger-ui-express");
 const morganBody = require("morgan-body");
+const openApiConfigration = require("./docs/swagger");
 const loggerStream = require("./utils/handleLogger");
 const dbConnectNoSql = require("./config/mongo");
 const { dbConnectMySql } = require("./config/mysql");
 const app = express();
-/**
- *
- */
+
 const ENGINE_DB = process.env.ENGINE_DB;
 const NODE_ENV = process.env.NODE_ENV || "development";
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
-
+/**
+ *
+ */
 morganBody(app, {
   noColors: true,
   stream: loggerStream,
   skip: function (req, res) {
-    return res.statusCode < 400; //TODO 2xx,3xx
+    return res.statusCode < 400;
   },
 });
-const port = process.env.PORT || 3000;
 
-/* Definir Ruta de la Documentacion */
+const port = process.env.PORT || 3000;
+/**
+ * Definir ruta de documentación
+ */
+
 app.use(
   "/documentation",
   swaggerUI.serve,
-  swaggerUI.setup(openApiConfiguration)
+  swaggerUI.setup(openApiConfigration)
 );
-/*sa
-Aqui invocamos a las rutas
-*/
 
+/**
+ * Aqui invocamos a las rutas! 😎
+ */
 app.use("/api", require("./routes"));
 
 if (NODE_ENV !== "test") {
